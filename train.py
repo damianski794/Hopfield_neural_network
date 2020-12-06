164239ca48c51496755e68823752f369e0aff21a
mikal05 <mikolaj.rzepinski@gmail.com>
2020-12-06 15:49:54 +0100

    added next thing

--- train.py
+++ train.py
@@ -24,18 +24,20 @@ def get_corrupted_input(input, corruption_level):
             corrupted[i] = -1 * v
     return corrupted
 
-def reshape(data):
-    dim = int(np.sqrt(len(data)))
-    data = np.reshape(data, (dim, dim))
+def reshape(data, width, height):
+    # dim = int(np.sqrt(len(data)))
+    data = np.reshape(data, (width, height))
     return data
 
-def plot(data, test, predicted, figsize=(5, 6)):
-    data = [reshape(d) for d in data]
-    test = [reshape(d) for d in test]
-    predicted = [reshape(d) for d in predicted]
+def plot(data, test, predicted, width, height, figsize=(5, 6)):
+    data = [reshape(d, width, height) for d in data]
+    test = [reshape(d, width, height) for d in test]
+    predicted = [reshape(d, width, height) for d in predicted]
 
-    fig, axarr = plt.subplots(len(data), 3, figsize=figsize)
-    for i in range(len(data)):
+    num_fig = 7 if len(data) > 7 else len(data)
+
+    fig, axarr = plt.subplots(num_fig, 3, figsize=figsize)
+    for i in range(num_fig):
         if i==0:
             axarr[i, 0].set_title('Train data')
             axarr[i, 1].set_title("Input data")
@@ -52,18 +54,19 @@ def plot(data, test, predicted, figsize=(5, 6)):
     plt.savefig("result.png")
     plt.show()
 
-def preprocessing(img, w=128, h=128):
-    # Resize image
-    img = resize(img, (w,h), mode='reflect')
-
-    # Thresholding
-    thresh = threshold_mean(img)
-    binary = img > thresh
-    shift = 2*(binary*1)-1 # Boolian to int
-
-    # Reshape
-    flatten = np.reshape(shift, (w*h))
-    return flatten
+# def preprocessing(img, w=128, h=128):
+#     # Resize image
+#     img = resize(img, (w,h), mode='reflect')
+#     print(img)
+#
+#     # Thresholding
+#     thresh = threshold_mean(img)
+#     binary = img > thresh
+#     shift = 2*(binary*1)-1 # Boolian to int
+#
+#     # Reshape
+#     flatten = np.reshape(shift, (w*h))
+#     return flatten
 
 def main():
     # Load data
@@ -71,22 +74,22 @@ def main():
     # astronaut = rgb2gray(skimage.data.astronaut())
     # horse = skimage.data.horse()
     # coffee = rgb2gray(skimage.data.coffee())
-    animals = dataload.load('data\\animals-14x9.csv')
-    large25 = dataload.load('data\\large-25x25.csv')
-    large25plus = dataload.load('data\\large-25x25.plus.csv')
-    large50 = dataload.load('data\\large-25x50.csv')
-    letters = dataload.load('data\\letters-14x20.csv')
-    lettersabc = dataload.load('data\\letters-abc-8x12.csv')
-    ocra = dataload.load('data\\OCRA-12x30-cut.csv')
-    small = dataload.load('data\\small-7x7.csv')
+    animals = dataload.load('data\\animals-14x9.csv')  # h = 9, w = 14
+    large25 = dataload.load('data\\large-25x25.csv')  # h = 25, w = 25
+    large25plus = dataload.load('data\\large-25x25.plus.csv')  # h = 25, w = 25
+    large50 = dataload.load('data\\large-25x50.csv')  # h = 50, w = 25
+    letters = dataload.load('data\\letters-14x20.csv')  # h = 20, w = 14
+    lettersabc = dataload.load('data\\letters-abc-8x12.csv')  # h = 12, w = 8
+    ocra = dataload.load('data\\OCRA-12x30-cut.csv')  # h = 30, w = 12
+    small = dataload.load('data\\small-7x7.csv')  # h = 7, w = 7
 
     # Marge data
-    data = [animals, large25, large25plus, large50, letters, lettersabc, ocra, small]
-
+    data = animals
+    height = 9
+    width = 14
     # Preprocessing
-    print("Start to data preprocessing...")
-    data = [preprocessing(d) for d in data]
-
+    # print("Start to data preprocessing...")
+    #data = [preprocessing(d, width, height) for d in data]
     # Create Hopfield Network Model
     model = network.HopfieldNetwork()
     model.train_weights(data)
@@ -94,9 +97,9 @@ def main():
     # Generate testset
     test = [get_corrupted_input(d, 0.3) for d in data]
 
-    predicted = model.predict(test, threshold=0, asyn=False)
+    predicted = model.predict(test, threshold=0, asyn=True)
     print("Show prediction results...")
-    plot(data, test, predicted)
+    plot(data, test, predicted, height, width)
     print("Show network weights matrix...")
     #model.plot_weights()
 
